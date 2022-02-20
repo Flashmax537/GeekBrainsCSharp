5ba331d72135b494d45ae7b073e83ac9ccdd0c2a
FlashMax <flashmax537@yandex.ru>
2022-02-20 22:57:40 +0400

    Выполнил практическое задание по уроку №7

---
+++ lesson7Task1/CheckForChange_dotPeek/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CheckForChange")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CheckForChange")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("466a3e91-66c9-47b9-ba2b-ed2be40a97d9")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
